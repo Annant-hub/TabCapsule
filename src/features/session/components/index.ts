ed12069a966425dc0f9d40434838818aacb4a01b
Anwar12 <annant448@gmail.com>
2026-07-16 11:11:17 +0530

    final-01

--- src/features/session/components/index.ts
+++ src/features/session/components/index.ts
@@ -1,5 +1,4 @@
 export { SaveWorkspaceButton } from "./SaveWorkspaceButton";
 export { SessionCard } from "./SessionCard";
 export { SessionList } from "./SessionList";
-export { SearchBox } from "./SearchBox";
 export { Toolbar } from "./Toolbar";
