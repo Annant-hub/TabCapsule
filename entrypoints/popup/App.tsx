8e4b815b88a92cc766382e7809134d37440c8d5d
Anwar12 <annant448@gmail.com>
2026-07-16 10:29:00 +0530

    logo-update

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
 
+import { useState } from "react";
+
+import logo from "../../src/assets/images/logo.png";
 import { useSessions } from "../../src/features/session/hooks";
 import { SaveWorkspaceButton } from "../../src/features/session/components/SaveWorkspaceButton";
 import { SessionList } from "../../src/features/session/components/SessionList";
 //import { SearchBox } from "../../src/features/session/components/SearchBox";
 import { Toolbar } from "../../src/features/session/components/Toolbar";
-import { useState } from "react";
+
 function App() {
   
   const session = useSessions();
@@ -24,17 +27,26 @@ const [sort, setSort] = useState<
     <main className="w-[420px] min-h-[600px] p-4 bg-[#0b0d1a] text-white">
       
       <header className="mb-6 border-b pb-4">
-  <h1 className="text-2xl font-bold tracking-tight text-white-900">
-    📦 TabCapsule
-  </h1>
+        <div className="flex items-center gap-3">
+          <img
+            src={logo}
+            alt="TabCapsule logo"
+            className="h-10 w-10 rounded-xl object-cover"
+          />
+          <div>
+            <h1 className="text-2xl font-bold tracking-tight text-white-900">
+              TabCapsule
+            </h1>
 
-  <p className="mt-1 text-sm text-gray-500 ">
-    Save and restore browser workspaces instantly
-  </p>
-  <p className="text-xs text-rose-300">
-    🐾 Love Animals
-  </p>
-</header>
+            <p className="mt-1 text-sm text-gray-500 ">
+              Save and restore browser workspaces instantly
+            </p>
+          </div>
+        </div>
+        <p className="mt-3 text-xs text-rose-300">
+          🐾 Love Animals
+        </p>
+      </header>
 
       <section className="mb-6 rounded-xl border  p-4 shadow-sm">
     <SaveWorkspaceButton
